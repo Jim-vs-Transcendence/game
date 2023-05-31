82395e7d46f24b0fabc1ed0acd60914732942678
Sesim <96904906+sob2545@users.noreply.github.com>
2023-05-31 17:55:02 +0900

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GameGateway } from './game.gateway';
 
 import { join } from 'path';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { GameGateway } from './game.gateway';
 
 @Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '../..', 'client'),
-    })
-  ],
-  controllers: [AppController],
-  providers: [AppService, GameGateway],
+	imports: [
+		ServeStaticModule.forRoot({
+			rootPath: join(__dirname, '../..', 'client'),
+		})
+	],
+	controllers: [AppController],
+	providers: [AppService, GameGateway]
 })
 export class AppModule { }
