a57b26f1d00b80da8b3ab85e9ab05e6bfe1988f6
Sesim <96904906+sob2545@users.noreply.github.com>
2023-05-31 17:54:25 +0900

    Update game.gateway.ts

--- src/game.gateway.ts
+++ src/game.gateway.ts
@@ -1,30 +1,44 @@
-import { WebSocketGateway, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage } from '@nestjs/websockets';
+import { Test } from '@nestjs/testing';
+import { WebSocketGateway, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, MessageBody } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway()
-export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
-    @WebSocketServer()
-    server: Server;
+export class GameGateway
+// implements OnGatewayConnection, OnGatewayDisconnect 
+{
+	@WebSocketServer() server: Server;
 
-    handleConnection(client: Socket) {
-        // 클라이언트 연결 이벤트 처리
-        console.log('Client connected:', client.id);
+	handleConnection(client: Socket) {
+		console.log('Client connected:', client.id);
 
-        // 클라이언트에게 연결 성공 메시지 보내기
-        client.emit('connected', { message: 'Connected to the server.' });
-    }
+		client.on('keydown', (keyCode: number) => {
+			// 클라이언트로부터 전달받은 키 다운 이벤트 처리
+			console.log('Keydown event:', keyCode);
+			// 로직 추가: 패들을 이동시키거나 게임 동작 처리 등
 
-    handleDisconnect(client: Socket) {
-        // 클라이언트 연결 해제 이벤트 처리
-        console.log('Client disconnected:', client.id);
-    }
+			// 다른 클라이언트에게 이벤트 전달
+			this.server.emit('keydown', { clientId: client.id, keyCode });
+		});
+	}
 
-    @SubscribeMessage('message')
-    handleMessage(client: Socket, payload: any) {
-        // 클라이언트로부터 메시지 수신 이벤트 처리
-        console.log('Received message:', payload);
+	// 	client.on('keyup', (keyCode: number) => {
+	// 		// 클라이언트로부터 전달받은 키 업 이벤트 처리
+	// 		console.log('Keyup event:', keyCode);
+	// 		// 로직 추가: 패들 이동 멈추기 등
 
-        // 클라이언트에게 응답 메시지 보내기
-        client.emit('response', { message: 'Message received.' });
-    }
+	// 		// 다른 클라이언트에게 이벤트 전달
+	// 		this.server.emit('keyup', { clientId: client.id, keyCode });
+	// 	});
+	// }
+	@SubscribeMessage("message")
+	name_ft(
+		@MessageBody() message: string
+	) {
+		console.log('Client next:', message);
+		this.server.emit("message", message);
+		// 로직 추가: 클라이언트가 연결 해제되었을 때 처리할 내용
+	}
+
+	
+
 }
