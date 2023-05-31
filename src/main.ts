944129e23ab309c4aa1bd79e680f54b8cb521514
Sesim <96904906+sob2545@users.noreply.github.com>
2023-05-31 17:55:21 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as socketIo from 'socket.io';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule);
+	app.setGlobalPrefix('api');
+	const server = app.getHttpServer();
+	const io = new socketIo.Server(server, { cors: { origin: 'http://175.117.47.114:1414' } });
 
-  // 정적 파일 서비스 활성화
+	// Socket.io 이벤트 및 로직을 처리하는 코드 작성
 
-  await app.listen(3000);
+	await app.listen(3000);
 }
 bootstrap();
