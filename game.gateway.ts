e5944cbfe97461b3b0cea602395a73ccd23a4a8b
Gyeong-Ju Kim <92440572+kkj-100-010-110@users.noreply.github.com>
2023-06-02 15:52:33 +0900

    Add files via upload

--- game.gateway.ts
+++ game.gateway.ts
@@ -1,59 +1,98 @@
 import { Test } from '@nestjs/testing';
 import { WebSocketGateway, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, MessageBody } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
-import { PlayerData } from './player.data';
+import { PlayerData, Players, Data } from './player.data';
 
 @WebSocketGateway()
 export class GameGateway
 // implements OnGatewayConnection, OnGatewayDisconnect 
 {
-	private players: PlayerData[] = [];
-	private playerId = 1;
+	private players: Players[] = [];
+
+	private canvasHeight: number = 800 * 0.8;
+	private paddleHeight: number = this.canvasHeight * 0.2;
+
+	private oneGame: Players = new Players();
 
 	@WebSocketServer() server: Server;
 
-
-	private initPlayer(player: PlayerData, clientId: string, id: number) {
+	private initPlayer(player: PlayerData, clientId: string) {
 		player.socket = clientId;
 		player.score = 0;
-		player.P1paddleY = 0;
-		player.P2paddleY = 0;
+		player.paddleY = 0;
 	}
-
 
 	handleConnection(client: Socket) {
 		console.log('Client connected:', client.id);
 
 		const playerData: PlayerData = new PlayerData();
-		this.initPlayer(playerData, client.id, this.playerId++);
-		this.players.push(playerData);
+		this.initPlayer(playerData, client.id);
+		console.log(this.oneGame.player1 ? 'wt' : 'fuck?');
+		if (!this.oneGame.player1) { // check
+			console.log('no player1')
+			this.oneGame.player1 = playerData;
+		}
+		else {
+			console.log('is in');
+			this.oneGame.player2 = playerData;
+			this.players.push(this.oneGame);
+		}
 
+		// key number가 필요 없을 듯.
+		// 화살표 아래 버튼 눌렀을 때
 		client.on('keydown', (data: { clientId: string, key: number }) => {
 			const { clientId, key } = data;
-			console.log('Who is this: ', data.clientId, data.key);
-			const player = this.players.find(player => player.socket === data.clientId);
-			if (player) {
-				console.log(player.socket)
-				if (data.key === 38)
-					player.P1paddleY -= 30;
-				else if (data.key === 40)
-					player.P1paddleY += 30;
-				// console.log('Player: ', player.socket);
-				// console.log('test: ', player.P1paddleY);
-				// console.log('=================');
+			console.log('is key downed? ', data);
+			if (this.oneGame.player1.socket === data.clientId) {
+				this.oneGame.player1.paddleY -= 30;
+				if (this.oneGame.player1.paddleY <= 0) {
+					this.oneGame.player1.paddleY = 0;
+				}
 			}
 			else {
-				console.log('we are fucked');
+				this.oneGame.player2.paddleY -= 30;
+				if (this.oneGame.player2.paddleY <= 0) {
+					this.oneGame.player2.paddleY = 0;
+				}
 			}
+			let tmp: Data = new Data();
+			console.log(this.players.length);
+			if (this.players.length === 1) {
+				console.log(this.oneGame.player1.socket, ' ', this.oneGame.player2.socket);
+				tmp.p1Paddle = this.oneGame.player1.paddleY;
+				tmp.p2Paddle = this.oneGame.player2.paddleY;
+				this.server.to(this.oneGame.player1.socket).emit('message', tmp);
+				this.server.to(this.oneGame.player2.socket).emit('message', tmp);
+			}
+		})
 
-			// if (this.players.length === 2) {
-			// game start
-			// console.log('Game started');
-			this.server.to(this.players[0].socket).emit('message', player);
-			this.server.to(this.players[1].socket).emit('message', player);
+		// 화살표 위 버튼 눌렀을 때
+		client.on('keyup', (data: { clientId: string, key: number }) => {
+			const { clientId, key } = data;
+			console.log('is key up? ', data);
+			if (this.oneGame.player1.socket === data.clientId) {
+				this.oneGame.player1.paddleY += 30;
+				if (this.oneGame.player1.paddleY >= this.canvasHeight - this.paddleHeight) {
+					this.oneGame.player1.paddleY = this.canvasHeight - this.paddleHeight;
+				}
+			}
+			else {
+				this.oneGame.player2.paddleY += 30;
+				if (this.oneGame.player2.paddleY >= this.canvasHeight - this.paddleHeight) {
+					this.oneGame.player2.paddleY = this.canvasHeight - this.paddleHeight;
+				}
+			}
+			let tmp: Data = new Data();
+			console.log(this.players.length);
+			if (this.players.length === 1) {
+				console.log(this.oneGame.player1.socket, ' ', this.oneGame.player2.socket);
+				tmp.p1Paddle = this.oneGame.player1.paddleY;
+				tmp.p2Paddle = this.oneGame.player2.paddleY;
+				this.server.to(this.oneGame.player1.socket).emit('message', tmp);
+				this.server.to(this.oneGame.player2.socket).emit('message', tmp);
+			}
+		})
 
-		});
-		// console.log('wtf');
 
 		// // ball 위치 계속 변경
 		// console.log('Keydowned event:', keyCode);
--- game.js
+++ game.js
@@ -17,8 +17,8 @@ document.addEventListener('DOMContentLoaded', () => {
 	const heightRatio = 0.8; // 세로 비율 (0~1 사이 값)
 
 	// 윈도우의 크기에 따라 Canvas의 너비와 높이 계산
-	const canvasWidth = 1600 * widthRatio;
-	const canvasHeight = 800 * heightRatio;
+	const canvasWidth = 800 * widthRatio;
+	const canvasHeight = 400 * heightRatio;
 
 	// Canvas의 너비와 높이 설정
 	canvas.width = canvasWidth;
@@ -42,6 +42,8 @@ document.addEventListener('DOMContentLoaded', () => {
 	let ball_moveX = false;
 	let ball_moveY = false;
 
+	const ballSpeed = 1;
+
 	// Paddle
 	const paddleWidth = canvasWidth * 0.02;
 	const paddleHeight = canvasHeight * 0.2;
@@ -62,9 +64,11 @@ document.addEventListener('DOMContentLoaded', () => {
 	const scoreTextSize = canvasHeight * 0.3;
 	const scoreMargin = canvasWidth * 0.2;
 
-
 	const score1X = canvasWidth / 2 - scoreMargin;
 	const score1Y = canvasHeight / 2 + (scoreTextSize * 3) / 8;
+
+	const score2X = canvasWidth / 2 + scoreMargin;
+	const score2Y = canvasHeight / 2 + (scoreTextSize * 3) / 8;
 
 	let left_score = 0;
 	let right_score = 0;
@@ -75,57 +79,86 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	function mainLogic() {
 		window.requestAnimationFrame(animate);
-		window.addEventListener('keydown', (key) => {
-			const data = {
-				clientId: socket.id,
-				key: key.keyCode
-			};
-			socket.emit('keydown', data)
-			console.log(data.clientId);
 
-			console.log(key.keyCode);
-			// let Player = {
-			// 	socket,
-			// 	score,
-			// 	P1paddleY,
-			// 	P2paddleY
-			// }
+		window.addEventListener('keydown', (key) => {
+			if (key.keyCode === 38) {
+				console.log('key down!')
+				const data = {
+					clientId: socket.id,
+					key: key.keyCode
+				};
+				socket.emit('keydown', data);
+			}
 		});
-		socket.on('message', (player) => {
-			// const { socket, score, P1paddleY, P2paddleY } = player;
-			/*
+		window.addEventListener('keyup', (key) => {
 			if (key.keyCode === 40) {
-				if (paddle1Y >= canvasHeight - paddleHeight)
-				paddle1Y = canvasHeight - paddleHeight;
-				paddle1Y += 30;
+				console.log('key up!')
+				const data = {
+					clientId: socket.id,
+					key: key.keyCode
+				};
+				socket.emit('keyup', data);
 			}
-			else if (key.keyCode === 38) {
-				if (paddle1Y <= 0)
-				paddle1Y = 0;
-				paddle1Y -= 30;
-			}
-			if (key.keyCode === 83) {
-				if (paddle2Y >= canvasHeight - paddleHeight)
-				paddle2Y = canvasHeight - paddleHeight;
-				paddle2Y += 30;
-			}
-			else if (key.keyCode === 87) {
-				if (paddle2Y <= 0)
-				paddle2Y = 0;
-				paddle2Y -= 30;
-			}
-			
-			=> 서버측 로직이 된다.
-			*/
-			console.log(player);
-			// console.log(player.P2paddleY);
-			paddle1Y += player.P1paddleY;
-			paddle2Y += player.P2paddleY;
 		});
+		socket.on('message', (paddleData) => {
+			console.log(paddleData);
+			paddle1Y += paddleData.p1Paddle;
+			paddle2Y += paddleData.p2Paddle;
+		});
+
+
+		// window.addEventListener('keydown', (key) => {
+		// 	const data = {
+		// 		clientId: socket.id,
+		// 		key: key.keyCode
+		// 	};
+		// 	socket.emit('keydown', data)
+		// 	console.log(data.clientId);
+
+		// 	console.log(key.keyCode);
+		// let Player = {
+		// 	socket,
+		// 	score,
+		// 	P1paddleY,
+		// 	P2paddleY
+		// }
+		// });
+
+		// socket.on('message', (player) => {
+		// 	// const { socket, score, P1paddleY, P2paddleY } = player;
+		// 	/*
+		// 	if (key.keyCode === 40) {
+		// 		if (paddle1Y >= canvasHeight - paddleHeight)
+		// 		paddle1Y = canvasHeight - paddleHeight;
+		// 		paddle1Y += 30;
+		// 	}
+		// 	else if (key.keyCode === 38) {
+		// 		if (paddle1Y <= 0)
+		// 		paddle1Y = 0;
+		// 		paddle1Y -= 30;
+		// 	}
+		// 	if (key.keyCode === 83) {
+		// 		if (paddle2Y >= canvasHeight - paddleHeight)
+		// 		paddle2Y = canvasHeight - paddleHeight;
+		// 		paddle2Y += 30;
+		// 	}
+		// 	else if (key.keyCode === 87) {
+		// 		if (paddle2Y <= 0)
+		// 		paddle2Y = 0;
+		// 		paddle2Y -= 30;
+		// 	}
+
+		// 	=> 서버측 로직이 된다.
+		// 	*/
+		// 	console.log(player);
+		// 	// console.log(player.P2paddleY);
+		// 	paddle1Y += player.P1paddleY;
+		// 	paddle2Y += player.P2paddleY;
+		// });
 	}
 
-	socket.on('connect', (data) => {
-		console.log();
+	socket.on('connect', () => {
+		// waiting function
 		mainLogic();
 	});
 
@@ -148,8 +181,18 @@ document.addEventListener('DOMContentLoaded', () => {
 	* drawing
 	*/
 	function reset_game() {
-		if (left_score === 3 || right_score === 3)
-			context.fillText('someone win', 0, 0);;
+		if (left_score === 3 || right_score === 3) {
+			console.log('is game win?');
+			// 게임이 끝나면 home버튼과 retry 버튼이 생기고 해당 문구가 출력되게
+			// 캔버스가 requestAnimationFrame 계속 그려질 필요 없이 해당 화면에서 멈추게
+			context.globalAlpha = 0.5;
+			context.font = '30px Arial';
+			context.fillStyle = 'white';
+			context.textAlign = 'center';
+			const textX = canvasWidth / 2;
+			const textY = canvasHeight / 2;
+			context.fillText('someone win', textX, textY);
+		}
 		ballX = init_ballX;
 		ballY = init_ballY;
 		paddle1Y = init_paddle1Y;
@@ -176,13 +219,13 @@ document.addEventListener('DOMContentLoaded', () => {
 			ball_moveY = true;
 
 		if (ball_moveY === true)
-			ballY -= 5; // check
+			ballY -= ballSpeed; // check
 		else if (ball_moveY === false)
-			ballY += 5;
+			ballY += ballSpeed;
 		if (ball_moveX === false)
-			ballX -= 5;
+			ballX -= ballSpeed;
 		else if (ball_moveX === true)
-			ballX += 5;
+			ballX += ballSpeed;
 
 		if (ballX - ballRadius * 2 <= init_paddle1X && ballX >= init_paddle1X - paddleWidth) {
 			if (ballY <= paddle1Y + paddleHeight && ballY >= paddle1Y) {
@@ -213,8 +256,6 @@ document.addEventListener('DOMContentLoaded', () => {
 		context.textAlign = 'center';
 		context.fillText(left_score, score1X, score1Y);
 
-		const score2X = canvasWidth / 2 + scoreMargin;
-		const score2Y = canvasHeight / 2 + (scoreTextSize * 3) / 8;
 		context.fillText(right_score, score2X, score2Y);
 
 		context.globalAlpha = 1;
@@ -227,9 +268,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 		context.fillStyle = 'white';
 		context.fillRect(paddle2X, paddle2Y, paddleWidth, paddleHeight);
+
 	}
-
-	// 공 그리기
-
 
 });
