9d61984c6de844fb8565e8d657730b82fc3fda7a
Sesim <96904906+sob2545@users.noreply.github.com>
2023-06-05 13:18:42 +0900

    Update game.gateway.ts

--- game.gateway.ts
+++ game.gateway.ts
@@ -9,9 +9,6 @@ export class GameGateway
 {
 	private players: Players[] = [];
 
-	private canvasHeight: number = 800 * 0.8;
-	private paddleHeight: number = this.canvasHeight * 0.2;
-
 	private oneGame: Players = new Players();
 
 	@WebSocketServer() server: Server;
@@ -27,13 +24,10 @@ export class GameGateway
 
 		const playerData: PlayerData = new PlayerData();
 		this.initPlayer(playerData, client.id);
-		console.log(this.oneGame.player1 ? 'wt' : 'fuck?');
 		if (!this.oneGame.player1) { // check
-			console.log('no player1')
 			this.oneGame.player1 = playerData;
 		}
 		else {
-			console.log('is in');
 			this.oneGame.player2 = playerData;
 			this.players.push(this.oneGame);
 		}
@@ -44,25 +38,30 @@ export class GameGateway
 			const { clientId, key } = data;
 			console.log('is key downed? ', data);
 			if (this.oneGame.player1.socket === data.clientId) {
-				this.oneGame.player1.paddleY -= 30;
-				if (this.oneGame.player1.paddleY <= 0) {
-					this.oneGame.player1.paddleY = 0;
-				}
+				this.oneGame.player1.paddleY = -30;
 			}
 			else {
-				this.oneGame.player2.paddleY -= 30;
-				if (this.oneGame.player2.paddleY <= 0) {
-					this.oneGame.player2.paddleY = 0;
-				}
+				this.oneGame.player2.paddleY = -30;
 			}
-			let tmp: Data = new Data();
+
+			let left: Data = new Data();
+			let right: Data = new Data();
 			console.log(this.players.length);
 			if (this.players.length === 1) {
 				console.log(this.oneGame.player1.socket, ' ', this.oneGame.player2.socket);
-				tmp.p1Paddle = this.oneGame.player1.paddleY;
-				tmp.p2Paddle = this.oneGame.player2.paddleY;
-				this.server.to(this.oneGame.player1.socket).emit('message', tmp);
-				this.server.to(this.oneGame.player2.socket).emit('message', tmp);
+
+				left.side = false;
+				left.p1Paddle = this.oneGame.player1.paddleY;
+				left.p2Paddle = this.oneGame.player2.paddleY;
+
+				right.side = true;
+				right.p1Paddle = this.oneGame.player2.paddleY;
+				right.p2Paddle = this.oneGame.player1.paddleY;
+
+				this.server.to(this.oneGame.player1.socket).emit('message', left);
+				this.server.to(this.oneGame.player2.socket).emit('message', right);
+				this.oneGame.player1.paddleY = 0;
+				this.oneGame.player2.paddleY = 0;
 			}
 		})
 
@@ -71,25 +70,29 @@ export class GameGateway
 			const { clientId, key } = data;
 			console.log('is key up? ', data);
 			if (this.oneGame.player1.socket === data.clientId) {
-				this.oneGame.player1.paddleY += 30;
-				if (this.oneGame.player1.paddleY >= this.canvasHeight - this.paddleHeight) {
-					this.oneGame.player1.paddleY = this.canvasHeight - this.paddleHeight;
-				}
+				this.oneGame.player1.paddleY = 30;
 			}
 			else {
-				this.oneGame.player2.paddleY += 30;
-				if (this.oneGame.player2.paddleY >= this.canvasHeight - this.paddleHeight) {
-					this.oneGame.player2.paddleY = this.canvasHeight - this.paddleHeight;
-				}
+				this.oneGame.player2.paddleY = 30;
 			}
-			let tmp: Data = new Data();
+			let left: Data = new Data();
+			let right: Data = new Data();
 			console.log(this.players.length);
 			if (this.players.length === 1) {
 				console.log(this.oneGame.player1.socket, ' ', this.oneGame.player2.socket);
-				tmp.p1Paddle = this.oneGame.player1.paddleY;
-				tmp.p2Paddle = this.oneGame.player2.paddleY;
-				this.server.to(this.oneGame.player1.socket).emit('message', tmp);
-				this.server.to(this.oneGame.player2.socket).emit('message', tmp);
+
+				left.side = false;
+				left.p1Paddle = this.oneGame.player1.paddleY;
+				left.p2Paddle = this.oneGame.player2.paddleY;
+
+				right.side = true;
+				right.p1Paddle = this.oneGame.player2.paddleY;
+				right.p2Paddle = this.oneGame.player1.paddleY;
+
+				this.server.to(this.oneGame.player1.socket).emit('message', left);
+				this.server.to(this.oneGame.player2.socket).emit('message', right);
+				this.oneGame.player1.paddleY = 0;
+				this.oneGame.player2.paddleY = 0;
 			}
 		})
 
@@ -108,4 +111,3 @@ export class GameGateway
 // 	this.server.emit("message", message);
 // 	// 로직 추가: 클라이언트가 연결 해제되었을 때 처리할 내용
 // }
-
